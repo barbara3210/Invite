5c07b5ebc162d348dfcdf9413f5ab7a17823ee02
barbara3210 <bivkovic@pmfst.hr>
2025-08-17 00:51:43 +0200

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,20 @@
-import Image from "next/image";
 import HodogramTimeline from "./components/HodogramTimeline";
-import { Great_Vibes } from "next/font/google";
-import InvitationNote from "./components/InvitationNote";
 import RSVPForm from "./components/RSVPForm";
 import Envelope from "./components/Envelope";
 import Hero from "./components/Hero";
-
-const greatVibes = Great_Vibes({ weight: "400", subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main className="bg-[#fffdf9] text-gray-800 min-h-screen">
       {/* Hero page */}
-      <Hero/>
+      <Hero />
 
-     
       {/* Pozivnica */}
-       <Envelope/>
-    
+      <Envelope />
+
       {/* Hodogram */}
       <section>
         <HodogramTimeline />
-       
       </section>
 
       {/* RSVPForm */}
