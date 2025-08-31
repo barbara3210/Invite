be45707e6f98a6f13def5c74975c055f8ad9a9ab
barbara3210 <bivkovic@pmfst.hr>
2025-08-31 23:42:52 +0200

    strelica fix

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -3,20 +3,25 @@ import Image from "next/image";
 export default function Hero() {
   return (
     <section className="relative h-screen w-full flex justify-center items-center overflow-hidden">
-      <Image
-        src="/dvajspete.jpg"
-        alt="Mato i Zrinka"
-        fill
-        className="object-cover"
-        priority
-      />
-       {/* Strelica koja sugerira scroll */}
-        <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
+      {/* Responsive hero slika */}
+      <div className="relative h-full w-full sm:h-screen sm:w-auto flex justify-center">
         <Image
-          src="/strelica.png"  
+          src="/dvajspete.jpg"
+          alt="Mato i Zrinka"
+          fill
+          className="object-cover object-center sm:object-contain"
+          priority
+        />
+      </div>
+
+      {/* Strelica koja sugerira scroll */}
+      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
+        <Image
+          src="/strelica.png"
           alt="Scroll dolje"
-          width={90}          
-          height={90}         
+          width={60}   
+          height={60}
+          className="sm:w-20 sm:h-20" 
         />
       </div>
     </section>
