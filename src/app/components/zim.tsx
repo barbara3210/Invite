8fde50aa8b362078e99ab4465f9e2480e5e61c73
barbara3210 <bivkovic@pmfst.hr>
2025-09-07 11:47:08 +0200

    hod/zim

--- src/app/components/zim.tsx
+++ src/app/components/zim.tsx
@@ -3,19 +3,19 @@ import Hod from "./hod";
 
 export default function Zim() {
   return (
-    <section className="relative min-h-[100dvh] w-full flex justify-center items-center overflow-hidden">
-      {/* Background image moved slightly up */}
-      <Image
-        src="/zim2.jpg"
-        alt="Mato i Zrinka"
-        fill
-        className="object-cover sm:object-contain"
-        style={{ objectPosition: "50% 20%" }} 
-        priority
-      />
+    <section className="relative min-h-[100dvh] w-full flex flex-col justify-end items-center overflow-visible">
+      {/* Background image container */}
+      <div className="relative w-full flex justify-center items-center flex-grow max-h-[80%] sm:max-h-[70%]">
+        <Image
+          src="/zim2.jpg"
+          alt="Mato i Zrinka"
+          fill
+          className="object-cover sm:object-contain sm:object-center"
+          priority
+        />
+      </div>
 
-      {/* Grid pinned to bottom */}
-      <div className="absolute bottom-3 w-full bg-white/80">
+      <div className="-mt-20 w-full bg-white/80">
         <Hod />
       </div>
     </section>
