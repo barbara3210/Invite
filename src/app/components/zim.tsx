1cb2e188d27726377e29b3e264fd459f9812cb48
barbara3210 <bivkovic@pmfst.hr>
2025-09-07 17:24:41 +0200

    move up

--- src/app/components/zim.tsx
+++ src/app/components/zim.tsx
@@ -5,17 +5,19 @@ export default function Zim() {
   return (
     <section className="relative min-h-[100dvh] w-full flex flex-col justify-end items-center overflow-visible">
       {/* Background image container */}
-      <div className="relative w-full flex justify-center items-center flex-grow max-h-[80%] sm:max-h-[70%]">
+      <div className="relative flex justify-center items-center flex-grow max-h-[50%] sm:max-h-[40%] w-full">
         <Image
-          src="/zim2.jpg"
-          alt="Mato i Zrinka"
-          fill
-          className="object-cover sm:object-contain sm:object-center"
-          priority
-        />
+  src="/zim1.jpg"
+  alt="Mato i Zrinka"
+  width={900}
+  height={700}
+  className="object-contain max-w-[90%] sm:max-w-[70%] h-auto"
+  priority
+/>
+
       </div>
 
-      <div className="-mt-20 w-full bg-white/80">
+      <div className=" mb-20 w-full bg-white/80">
         <Hod />
       </div>
     </section>
